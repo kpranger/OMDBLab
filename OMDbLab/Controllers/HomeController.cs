b3c7ebde5f9afceac73b4039045b4ed110536e58
Kris <pranger.kris@gmail.com>
2022-07-26 18:53:04 -0400

    OMDBLab

---
+++ OMDbLab/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OMDbLab.Models;
+using System.Diagnostics;
+
+namespace OMDbLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public MovieDAL movieDAL { get; set; } = new MovieDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]     //run when you first go to page
+        public IActionResult MovieSearch()
+        {
+            return View();
+        }
+
+        [HttpPost]     //run when you submit your form
+        public IActionResult MovieSearch(string title)
+        {
+            MovieModel movie = movieDAL.GetMovie(title);
+            return View(movie);
+        }
+
+        [HttpGet]     //run when you first go to page
+        public IActionResult MovieNightForm()
+        {
+            return View();
+        }
+
+        [HttpPost]     //run when you submit your form
+        public IActionResult MovieNightResults(string title1, string title2, string title3)
+        {
+            List<MovieModel> results = new List<MovieModel>
+            {
+                movieDAL.GetMovie(title1),
+                movieDAL.GetMovie(title2),
+                movieDAL.GetMovie(title3)
+            };
+            return View(results);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OMDbLab/Models/MovieDAL.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace OMDbLab.Models
+{
+    public class MovieDAL
+    {
+        public MovieModel GetMovie(string title)
+        {
+            string url = $"http://www.omdbapi.com/?apikey=aaf8a22b&t={title}";
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+            MovieModel result = JsonConvert.DeserializeObject<MovieModel>(JSON);
+            return result;
+        }
+    }
+}
